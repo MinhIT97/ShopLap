acf5a995b6b3728fb033c817972352e446ffc5d5
MinhIT97 <54066831+MinhIT97@users.noreply.github.com>
2019-10-10 21:09:26 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LapShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
